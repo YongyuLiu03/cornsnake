e370da5d5bab140c36950864668b7ca32b555f76
Yongyu Liu <yl8126@nyu.edu>
2023-04-17 09:41:27 -0400

    milestone3

--- client/src/pages/Mine.js
+++ client/src/pages/Mine.js
@@ -1,8 +1,11 @@
 import {useContext, useEffect} from "react";
 import {Navigate} from "react-router-dom";
+import {Image} from "../components/Img";
 
 function Mine() {
-
+    return (
+        <Image />
+        )
 }
 
 export default Mine;
